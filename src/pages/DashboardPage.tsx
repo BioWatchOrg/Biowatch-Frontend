b70422603ede78782d0be82dcabbe8da1795d30d
yarong <yarong.meng@epitech.eu>
2026-06-24 14:54:46 +0200

    refactor: route logout through auth session adapter

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { clearDummySession } from '../features/auth/utils/dummyAuth';
+import { clearAuthSession } from '../features/auth/services/authSession';
 
 function DashboardPage() {
   return (
@@ -14,7 +14,7 @@ function DashboardPage() {
         <Link
           to="/login"
           className="auth-button auth-button-secondary"
-          onClick={() => clearDummySession()}
+          onClick={() => clearAuthSession()}
         >
           Se deconnecter
         </Link>
